8c645272560870b8405d257d3549bcade39fe949
Karol Wróbel <karol.wrobel@nordcloud.com>
2022-01-06 17:42:34 +0100

    main

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -13,26 +13,26 @@ provider "azurerm" {
   version = "~>2.0"
   features {}
 }
- 
+
 data "azurerm_client_config" "current" {}
- 
+
 #Create Resource Group
 resource "azurerm_resource_group" "tamops" {
-  name     = "kw-rg-tf"
-  location = "westeurope"
+  name     = "tamops"
+  location = "eastus2"
 }
- 
+
 #Create Virtual Network
 resource "azurerm_virtual_network" "vnet" {
-  name                = "kww-vnet-tf"
+  name                = "tamops-vnet"
   address_space       = ["192.168.0.0/16"]
   location            = "eastus2"
   resource_group_name = azurerm_resource_group.tamops.name
 }
- 
+
 # Create Subnet
 resource "azurerm_subnet" "subnet" {
-  name                 = "kw-subnet-tf"
+  name                 = "subnet"
   resource_group_name  = azurerm_resource_group.tamops.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefix       = "192.168.0.0/24"
